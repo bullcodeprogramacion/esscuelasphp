0db2927d3d3a8b29397f6d5eb0457e8d98ec507c
bullcodeProgramacion <bullcodeprogramacion@gmail.com>
2022-04-05 22:50:01 +0100

    creacion de clases

--- gestion_escuelas.php
+++ gestion_escuelas.php
@@ -1,6 +1,8 @@
 <?php
     require_once './controllers/loginController.php';
+    require_once './controllers/gestionController.php';
     checkSession($_SERVER['REQUEST_URI']);
+    if(isset($_POST['enviar'])) $response = crearClase($_POST['escuela_nombre'],$_POST['url_escuela']);
     require_once 'head.php';
 ?>
     <header>
@@ -16,6 +18,9 @@
             <input type="text" name="escuela_nombre" placeholder="Inserta el nombre de la escuela">
             <input type="text" name="url_escuela" placeholder="Inserta la url">
             <input type="submit" value="Crear Escuela" name="enviar">
+            <?php
+                if(isset($response) && $response=="Error al crear clase") echo "<span class='error'>$response</span>";
+            ?>
         </form>
     </div>
 </body>
