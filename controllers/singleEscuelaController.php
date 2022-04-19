6571eae103d9050032518a31fadd0ffa83b024cc
bullcodeProgramacion <bullcodeprogramacion@gmail.com>
2022-04-19 14:23:30 +0100

    insertar usuarios y clases

--- controllers/singleEscuelaController.php
+++ controllers/singleEscuelaController.php
@@ -6,4 +6,37 @@
         return $data;
     }
 
+    function crearClase($idEscuela,$nombre_clase,$dataEscuela,$data){
+        if(strlen($nombre_clase)<3) return "Error al crear clase";
+        array_push($dataEscuela->clases,$nombre_clase);
+        return crearData($dataEscuela,$idEscuela,$data);
+    }
+
+    function crearAlumno($idEscuela,$dataAlumno,$dataEscuela,$data){
+        if(strlen($dataAlumno['nombre_alumno'])<3 || strlen($dataAlumno['apellidos_alumno'])<3) return "Error al crear clase";
+        $datosAlumno = [
+            "nombre" => $dataAlumno['nombre_alumno'],
+            "apellidos" => $dataAlumno['apellidos_alumno'],
+            "email" =>  $dataAlumno['email']
+        ];
+        array_push($dataEscuela->alumnos,$datosAlumno);
+        return crearData($dataEscuela,$idEscuela,$data);
+    }
+
+    function crearData($dataEscuela,$idEscuela,$data){
+        $dataEscuela = json_encode($dataEscuela);
+        $posicion = $idEscuela - 1;
+        $data[$posicion] = $dataEscuela;
+
+        /// pasamos todos los datos a string con nuestro delimitador
+        $data = implode(";",$data);
+        // apuntamos a nuestro archivo creado
+        $dataFile = fopen("clases/clases.txt","w");
+        // insertamos nuestro array
+        fwrite($dataFile,$data.";");
+        // cerramos el archivo
+        fclose($dataFile);
+        return true;
+    }
+
 ?>
--- escuela.php
+++ escuela.php
@@ -7,6 +7,8 @@
     $idEscuela = $_GET['id'];
     $data = obtenerTodosLosDatos();
     $dataEscuela = obtenerEscuela($data,$idEscuela);
+    if(isset($_POST['enviar_clase'])) crearClase($idEscuela,$_POST['nombre_clase'],$dataEscuela,$data);
+    if(isset($_POST['enviar_alumno'])) crearAlumno($idEscuela,$_POST,$dataEscuela,$data);
 ?>
 <div class="data-escuela">
     <p>Imagen de escuela</p>
@@ -14,6 +16,7 @@
     <p> Nombre : <span> <?php echo $dataEscuela->nombre ?></span></p>
     <p>Sitio web : <a href="https://<?php echo $dataEscuela->url ?>" target="_blank"> <?php echo $dataEscuela->url ?></a></p>
 </div>
+<hr>
 <div class="insert-data">
     <h1>Crear Clase</h1>
     <form action='' method='POST'>
@@ -27,6 +30,27 @@
         <input type="text" name="nombre_alumno" placeholder="Insertar el nombre del alumno">
         <input type="text" name="apellidos_alumno" placeholder="Insertar el apellido del alumno">
         <input type="mail" name="email" placeholder="Insertar el email del alumno">
-        <input type="submit" name="enviar_clase" value="Crear Alumno">
+        <input type="submit" name="enviar_alumno" value="Crear Alumno">
     </form>
+</div>
+<hr>
+<h2 class="title-data">DATOS DE LA ESCUELA <?php echo $dataEscuela->nombre?></h2>
+<div class="show-data">
+    <h3>Todas las clases</h3>
+    <?php
+        foreach ($dataEscuela->clases as $clase) {?>
+            <span class="each-escuela"><?php echo $clase?></span> 
+    <?php }
+    ?>
+</div>
+<div class="show-data">
+    <h3>Todos los alumnos</h3>
+    <?php
+        foreach ($dataEscuela->alumnos as $alumno) {?>
+            <div class="each-data-alumno">
+                <span class="each-alumno">Nombre y Apellidos: <?php echo $alumno->nombre." ".$alumno->apellidos?></span>
+                <span class="each-alumno">Email: <?php echo $alumno->email?></span>
+            </div> 
+    <?php }
+    ?>
 </div>
